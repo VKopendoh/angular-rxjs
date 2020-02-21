1d0972aee2b4aaa154559a87eede64a4eff11b06
vkopendoh <vkopendoh@bk.ru>
2020-02-21 11:25:47 +0300

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {interval, Subscription, Observable, Subject} from 'rxjs';
+import {map, filter, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-rxjs';
+  sub: Subscription;
+
+  sub2: Subscription;
+
+  sub3: Subscription;
+
+  stream$: Subject<number> = new Subject<number>();
+
+  counter = 0;
+
+  constructor() {
+    this.sub3 = this.stream$.subscribe(val => {
+      console.log('Subcribe" ', val);
+    });
+
+    const stream$ = new Observable(obs => {
+      setTimeout(() => {
+        obs.next(1);
+      }, 500);
+
+      setTimeout(() => {
+        obs.complete();
+      }, 1900);
+
+      setTimeout(() => {
+        obs.error('Error 2 secs');
+      }, 2000);
+
+      setTimeout(() => {
+        obs.next(2);
+      }, 2500);
+    });
+
+    this.sub2 = stream$.subscribe(value => {
+        console.log('Next: ', value);
+      }, error => console.log('Error: ', error),
+      () => console.log('Stream complete!'));
+
+    const intervalStream$ = interval(1000);
+
+    this.sub = intervalStream$
+      .pipe(
+        filter(value => value % 2 === 0),
+        map((val) => `Mapped value ${val}`),
+        switchMap(() => interval(500))
+      )
+      .subscribe((value) => {
+        console.log(value);
+      });
+  }
+
+  stop() {
+    this.sub.unsubscribe();
+  }
+
+  stop2() {
+    this.sub3.unsubscribe();
+  }
+
+  next() {
+    this.counter++;
+    this.stream$.next(this.counter);
+  }
 }
